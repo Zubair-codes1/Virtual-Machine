4dcec317b006759b6854b3da4da93ab587d3d57e
Zubair <zubairabdulmatin@gmail.com>
2026-07-25 17:21:01 +0100

    START: working on code generator

---
+++ compiler/src/zplusplus/code_gen/CodeGenerator.java
@@ -0,0 +1,10 @@
+package zplusplus.code_gen;
+
+/**
+ * Class to handle code generation into my custom VM
+ * assembly language (ZASM).
+ *
+ * @author Zubair Abdul Matin
+ */
+public class CodeGenerator {
+}

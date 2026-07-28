6951b2b06c836ef55b22827897c078b9f136d881
Zubair <zubairabdulmatin@gmail.com>
2026-07-28 16:10:31 +0100

    FIX: changing the variable symbol to not hold the slot anymore (the environment will handle it)

--- compiler/src/zplusplus/sem_analysis/symbol/VariableSymbol.java
+++ compiler/src/zplusplus/sem_analysis/symbol/VariableSymbol.java
@@ -8,18 +8,13 @@ import zplusplus.sem_analysis.Type;
  * @author Zubair Abdul Matin
  */
 public class VariableSymbol extends Symbol {
-
-    private int slot;
-
     /**
      * Variable Symbol constructor
      * @param name variable name
      * @param type variable type
      */
-    public VariableSymbol(String name, Type type, int slot) {
+    public VariableSymbol(String name, Type type) {
         super(name, type);
-
-        this.slot = slot;
     }
 
     /**

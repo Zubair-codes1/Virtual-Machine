a2f0e00abe919d71c449496eda4d432330c607e4
Zubair <zubairabdulmatin@gmail.com>
2026-08-10 15:43:49 +0100

    TESTING: virtual machine testing update to consider full compiler pipeline programs

--- vm/tests/VirtualMachineTest.java
+++ vm/tests/VirtualMachineTest.java
@@ -1,9 +1,27 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
+/**
+ * Class to test the full virtual machine workflow, including
+ * binary files that are assembled to only, and those that go through
+ * the full compiler -> asssembler -> binary execution flow.
+ *
+ * @author Zubair Abdul Matin
+ */
 public class VirtualMachineTest {
 
     @Test
+    @DisplayName("Testing compiler program run on virutal machine")
+    void testCompilerExecutionFull() {
+        VirtualMachine vm = new VirtualMachine(false);
+        vm.loadFromBinary("../output/compilerTest.bin");
+        assertDoesNotThrow(vm::executeProgram);
+    }
+
+    @Test
+    @DisplayName("Testing factorial for errors")
     void testFactorialExecutesWithoutError() {
         VirtualMachine vm = new VirtualMachine(false);
         vm.loadFromBinary("../output/factorial.bin");
@@ -11,6 +29,7 @@ public class VirtualMachineTest {
     }
 
     @Test
+    @DisplayName("Testing fibonacci for errors")
     void testFibonacciExecutesWithoutError() {
         VirtualMachine vm = new VirtualMachine(false);
         vm.loadFromBinary("../output/fibonacci.bin");
@@ -18,6 +37,7 @@ public class VirtualMachineTest {
     }
 
     @Test
+    @DisplayName("Testing hello world for errors")
     void testHelloWorldExecutesWithoutError() {
         VirtualMachine vm = new VirtualMachine(false);
         vm.loadFromBinary("../output/hello_world.bin");
@@ -25,6 +45,7 @@ public class VirtualMachineTest {
     }
 
     @Test
+    @DisplayName("Testing storage of global variables")
     void testGlobalVariableStoredCorrectly() {
         VirtualMachine vm = new VirtualMachine(false);
         vm.loadFromBinary("../output/factorial.bin");
@@ -33,6 +54,7 @@ public class VirtualMachineTest {
     }
 
     @Test
+    @DisplayName("Testing invalid binary files")
     void testInvalidBinaryThrows() {
         VirtualMachine vm = new VirtualMachine(true);
         assertThrows(VirtualMachineException.class, () -> {
@@ -41,6 +63,7 @@ public class VirtualMachineTest {
     }
 
     @Test
+    @DisplayName("Testing for stack emptiness after halt has run")
     void testStackIsEmptyAfterHalt() {
         VirtualMachine vm = new VirtualMachine(false);
         vm.loadFromBinary("../output/factorial.bin");

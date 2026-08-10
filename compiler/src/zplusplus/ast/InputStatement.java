21c0b153476fef41caea3f2f070a89d78db593dc
Zubair <zubairabdulmatin@gmail.com>
2026-08-10 16:07:38 +0100

    ADD: input statement ast node

--- compiler/src/zplusplus/ast/InputStatement.java
+++ compiler/src/zplusplus/ast/InputStatement.java
@@ -1,4 +1,42 @@
 package zplusplus.ast;
 
-public class InputStatement {
+/**
+ * Input statement ast node
+ *
+ * @author Zubair Abdul Matin
+ */
+public class InputStatement extends Statement {
+
+    // variable that the input will be stored in
+    private String variable;
+
+    /**
+     * Constructor for input statement to initialise the
+     * variable
+     * @param variable variable that input will be set to
+     * @param lineNumber line number of input statement
+     */
+    public InputStatement(String variable, int lineNumber) {
+        super(lineNumber);
+
+        this.variable = variable;
+    }
+
+    /**
+     * variable getter
+     * @return variable
+     */
+    public String getVariable() {
+        return variable;
+    }
+
+    /**
+     * Converts input statement to string for better
+     * debugging and error messages
+     * @return input statement string
+     */
+    @Override
+    public String toString() {
+        return "input(" + variable + ");";
+    }
 }
